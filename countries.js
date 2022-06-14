2ea8e2ca82913c926239764980f9fc04aad13ab2
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-06-14 19:42:51 +0600

    done

--- countries.js
+++ countries.js
@@ -5,7 +5,20 @@ const loadCountries = () =>{
 }
 
 const displayCountries = countries =>{
-    console.log(countries);
+    console.log(countries[0]);
+    const countriesHTML = countries.map(country =>getCountryHTML(country));
+    // console.log(CountriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+}
 
-} 
+const getCountryHTML = country =>{
+    return `
+        <div class="country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>   
+    `
+}
+
 loadCountries();
